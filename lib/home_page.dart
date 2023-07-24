0449c9da410bdef08962be2c7f72d222895bc96e
ghajavarestani <ermiailia@gmail>
2023-07-24 17:12:09 +0330

    payane film 37th shoroe film 38th row

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,4 @@
-
-
 import 'package:flutter/material.dart';
-
 
 //film 33&34&35&36th
 class HomePage extends StatefulWidget {
@@ -68,40 +65,91 @@ class _HomePageState extends State<HomePage> {
       //     print('welcome to flutter');
       //     },
       //   child: Text('Welcome'),),
-      body: Center(
-        child: Container(
-          // color: Colors.green,
-          height: 120,
-          width: 220,
-          child: Text('Welcome'),
-          decoration: BoxDecoration(
-            // color: Colors.green
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.topRight,
-              colors: [
-                // Colors.purple.withOpacity(0.9),
-                // Colors.amber.withOpacity(0.5)
-                Colors.purple,
-                Colors.amber
-              ]
-            ),
-            //borderRadius: BorderRadius.all(Radius.circular(20))
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40),
-              topLeft: Radius.circular(20)
+      body:
+
+      Container(
+        width: 400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //mainAxisAlignment: MainAxisAlignment.start,
+          //mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              // color: Colors.green,
+              height: 120,
+              width: 220,
+              child: Text('Welcome'),
+              decoration: BoxDecoration(
+                // color: Colors.green
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.topRight,
+                      colors: [
+                        // Colors.purple.withOpacity(0.9),
+                        // Colors.amber.withOpacity(0.5)
+                        Colors.purple,
+                        Colors.amber
+                      ]
+                  ),
+                  //borderRadius: BorderRadius.all(Radius.circular(20))
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40),
+                      topLeft: Radius.circular(20)
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.amber,
+                        blurRadius: 10.0,
+                        //blurStyle:
+                        offset: Offset(10,10)
+                    )
+                  ]
+                //payane film 36th va shoroue film 37th
               ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.amber,
-                blurRadius: 10.0,
-                //blurStyle:
-                offset: Offset(10,10)
-              )
-            ]
-              //payane film 36th va shoroue film 37th
-          ),
+            ),
+            ElevatedButton(onPressed: (){}, child: Text('Column1')),
+            ElevatedButton(onPressed: (){}, child: Text('Column2'))
+          ],
         ),
-      ),
-    );
+      )
+      // body: Center(
+      //   child: Container(
+      //     // color: Colors.green,
+      //     height: 120,
+      //     width: 220,
+      //     child: Text('Welcome'),
+      //     decoration: BoxDecoration(
+      //       // color: Colors.green
+      //       gradient: LinearGradient(
+      //         begin: Alignment.topCenter,
+      //         end: Alignment.topRight,
+      //         colors: [
+      //           // Colors.purple.withOpacity(0.9),
+      //           // Colors.amber.withOpacity(0.5)
+      //           Colors.purple,
+      //           Colors.amber
+      //         ]
+      //       ),
+      //       //borderRadius: BorderRadius.all(Radius.circular(20))
+      //         borderRadius: BorderRadius.only(bottomLeft: Radius.circular(40),
+      //         topLeft: Radius.circular(20)
+      //         ),
+      //       boxShadow: [
+      //         BoxShadow(
+      //           color: Colors.amber,
+      //           blurRadius: 10.0,
+      //           //blurStyle:
+      //           offset: Offset(10,10)
+      //         )
+      //       ]
+      //         //payane film 36th va shoroue film 37th
+      //     ),
+      //   ),
+      );
+    //payane film 37th shoroe film 38th row
   }
 }
